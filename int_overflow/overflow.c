e5f35d142f706dee227f247fecea21acca74fcf7
Wolfgang <wolfgang.spindeler@gmail.com>
2022-09-30 10:24:50 +0200

    clean

--- int_overflow/overflow.c
+++ int_overflow/overflow.c
@@ -5,10 +5,7 @@ int main(){
 	printf("Max values:\n\tINT_MAX:\t%i\n",INT_MAX);
 	printf("\tLONG_MAX:\t%li\n\n",LONG_MAX);
 
-	int a = 200;
-	int b = 300;
-	int c = 400;
-	int d = 500;
+	int a = 200, b = 300, c = 400, d = 500;
 
 	int result_int = a * b * c * d;
 	long result_long = (long) a * (long) b * (long) c * (long) d; 

6869b228dda76957d2a3e387b9540bbfe45ad25e
Wolfgang <wolfgang.spindeler@gmail.com>
2022-09-30 13:27:07 +0200

    memory addresses and byte repreesentation

---
+++ memory_addreesses/mem.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+unsigned int a = 42;
+unsigned int size_of_a = sizeof(a);
+
+void main(){
+	printf("Value of a: %d\n", a);
+	printf("Memory addreess of a: %p\n", &a);
+	printf("Bytes occupied by a: %d\n", size_of_a);
+	printf("Byte representation of a: ");
+       	print_unsignd_int(a);
+	printf("\n\n");
+}
+
+
+void print_bytes(void *p, size_t len)
+{
+    size_t i;
+    printf("(");
+    for (i = 0; i < len; ++i)
+        printf("%02X", ((unsigned char*)p)[i]);
+    printf(")");
+}
+
+
+void print_unsignd_int(unsigned int x)
+{
+    print_bytes(&x, sizeof(x));
+}
